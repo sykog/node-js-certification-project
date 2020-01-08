91fd0b9a1952615f9effbc5c12a2b258b702f44f
oulasawas <oula.sawas92@gmail.com>
2020-01-08 11:12:40 -0500

    adding sign in and sign up page not finished

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,21 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HttpClientModule} from "@angular/common/http";
 
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home/home.component';
 import { WeatherComponent } from './home/weather/weather.component';
+import { LoginComponent } from './Admin/login/login.component';
+import { SignupComponent } from './Admin/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    WeatherComponent
+    WeatherComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
